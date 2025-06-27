64e51cacda83a737cf934bfcc704588d76100d07
Ranevskaya Anna Dmitrievna <akhamylova@ozon.ru>
2025-06-27 14:20:48 +0300

    Done task 2

--- mainTest.py
+++ mainTest.py
@@ -1,9 +1,17 @@
 
-class MainTest():
+class MainClass():
+    __class_number = 20
+
     def get_local_number(self) -> int:
         return 14
 
+    def get_class_number(self):
+        return self.__class_number
 
-class MainClassTest(MainTest):
+
+class MainClassTest(MainClass):
     def test_get_local_number(self) -> None:
-        assert MainTest().get_local_number() == 14, "Метод get_local_number() должен возвращать 14"
+        assert MainClass.get_local_number() == 14, "Метод get_local_number() должен возвращать 14"
+
+    def testGetClassNumber(self):
+        assert MainClass.get_class_number() > 45, "Метод get_class_number() должен возвращать число больше 45"
