7ef9ac65233d2e91c7f1043c9cb7ff3392fec78d
Ranevskaya Anna Dmitrievna <akhamylova@ozon.ru>
2025-07-27 15:17:50 +0300

    Refactor test (refactor using fixtures)

--- test_first_test.py
+++ test_first_test.py
@@ -3,27 +3,10 @@ from appium.options.android import UiAutomator2Options
 import pytest
 
 
-@pytest.mark.usefixtures("driver_setup_teardown")
 class TestFirst:
-
-    @pytest.fixture()
-    def driver_setup_teardown(self):
-        options = UiAutomator2Options().load_capabilities(
-        {
-            "platformName": "Android",
-            "appium:deviceName": "emulator-5554",
-            "appium:platformVersion": "15.0",
-            "appium:automationName": "UiAutomator2",
-            "appium:appPackage": "org.wikipedia",
-            "appium:appActivity": ".main.MainActivity",
-            "appium:app": "/Users/akhamylova/PycharmProjects/PythonAppiumAutomation/org.wikipedia.apk"
-        }
-            )
-        self.driver = webdriver.Remote("http://127.0.0.1:4723/wd/hub", options=options)
-        print('setUp completed')
-        yield
-        self.driver.quit()
-        print('teardown completed')
+    @pytest.fixture(autouse=True)
+    def __setup_class(self, driver_setup_teardown):
+        self.driver = driver_setup_teardown
 
     def test_first(self):
         """Пример теста."""
