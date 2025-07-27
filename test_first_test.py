beb617e749510468f1a283b20e24c52e7cab1954
Ranevskaya Anna Dmitrievna <akhamylova@ozon.ru>
2025-07-27 13:01:59 +0300

    Refactor first_test

--- test_first_test.py
+++ test_first_test.py
@@ -1,9 +1,13 @@
 from appium import webdriver
 from appium.options.android import UiAutomator2Options
+import pytest
 
 
-class FirstTest:
-    def __init__(self):
+@pytest.mark.usefixtures("driver_setup_teardown")
+class TestFirst:
+
+    @pytest.fixture()
+    def driver_setup_teardown(self):
         options = UiAutomator2Options().load_capabilities(
         {
             "platformName": "Android",
@@ -12,14 +16,16 @@ class FirstTest:
             "appium:automationName": "UiAutomator2",
             "appium:appPackage": "org.wikipedia",
             "appium:appActivity": ".main.MainActivity",
-            "appium:app": "/Users/akhamylova/PycharmProjects/pythonProject1/org.wikipedia.apk"
+            "appium:app": "/Users/akhamylova/PycharmProjects/PythonAppiumAutomation/org.wikipedia.apk"
         }
-        )
+            )
         self.driver = webdriver.Remote("http://127.0.0.1:4723/wd/hub", options=options)
-        print('first test run')
-
-    def __del__(self):
+        print('setUp completed')
+        yield
         self.driver.quit()
+        print('teardown completed')
 
-
-first_test_init = FirstTest()
+    def test_first(self):
+        """Пример теста."""
+        print(f'run first test')
+        assert self.driver is not None, "Драйвер не инициализирован!"
