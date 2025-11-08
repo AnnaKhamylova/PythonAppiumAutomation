db70e599f7b0210a718adf5f8692c520292aa634
Ranevskaya Anna Dmitrievna <akhamylova@ozon.ru>
2025-11-08 19:52:41 +0300

    Переименовала класс

--- test_for_ios.py
+++ test_for_ios.py
@@ -2,7 +2,7 @@
 import pytest
 
 
-class TestAndroid:
+class TestIos:
     def test_swipe_ios(self, set_up, main_page, portrait, welcome_page):
         welcome_page.first_page()
         welcome_page.swipe_left(0.1)
